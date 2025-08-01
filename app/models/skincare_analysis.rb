acc752f563c927e19e7d786f01edd5af39ebc466
Donald Lee <donaldlee50@gmail.com>
2025-07-31 19:19:16 -0500

    Fix backblaze deletion code

--- app/models/skincare_analysis.rb
+++ app/models/skincare_analysis.rb
@@ -3,18 +3,10 @@ class SkincareAnalysis < ApplicationRecord
   validates :image_url, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
 
+  after_destroy :delete_image_from_backblaze
 
-  def delete_backblaze_object_from_url
+  def delete_image_from_backblaze
     return unless image_url.present?
-  
-    bucket_name = Rails.application.credentials.dig(Rails.env.to_sym, :backblaze, :bucket_name)
-    object_key = image_url.split("https://f005.backblazeb2.com/file/#{bucket_name}/").last
-  
-    s3_client.delete_object(
-      bucket: bucket_name,
-      key: object_key
-    )
-  rescue StandardError => e
-    Rails.logger.error("Failed to delete Backblaze object: #{e.message}")
+    delete_from_backblaze
   end
 end
